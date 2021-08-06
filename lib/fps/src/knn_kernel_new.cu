7887be1f75ebdafeeda13a4ffb17802c006cf178
072jiajia <072jiajia@gmail.com>
2021-08-07 00:12:15 +0800

    fix bug

--- lib/fps/src/knn_kernel_new.cu
+++ lib/fps/src/knn_kernel_new.cu
@@ -9,22 +9,21 @@
 #include <cuda_runtime.h>
 
 
+#define __update(dists, dists_i, idx1, idx2) \
+  const float v1 = dists[idx1], v2 = dists[idx2]; \
+  if (v2 < v1) { \
+    dists[idx1] = v2; \
+    dists_i[idx1] = dists_i[idx2]; \
+  }
 
 #define max_parallel(bs, id) \
   if (block_size >= bs) { \
     if (tid < id) { \
-      __update(dists, dists_i, tid, tid + id); \
+      const long tid2 = tid + id; \
+      __update(dists, dists_i, tid, tid2); \
     } \
     __syncthreads(); \
   }
-
-__device__ void __update(float *__restrict__ dists, long *__restrict__ dists_i, const long idx1, const long idx2) {
-  const float v1 = dists[idx1], v2 = dists[idx2];
-  if (v2 < v1) {
-    dists[idx1] = v2;
-    dists_i[idx1] = dists_i[idx2];
-  }
-}
 
 
 #define MAX_THREADS 1024
@@ -37,20 +36,20 @@ __global__ void furthest_point_sampling_kernel(int n, int m, int K,
   __shared__ long dists_i[MAX_THREADS];
 
   int tid = threadIdx.x;
-  long block_size = gridDim.x;
+  long block_size = blockDim.x;
 
-  for (long i = blockIdx.x; i < m; i += gridDim.x) {
+  for (long i = blockIdx.x; i < n; i += gridDim.x) {
     long index1 = i * 3;
     float x1 = points[index1];
     float y1 = points[index1 + 1];
     float z1 = points[index1 + 2];
 
     long last_besti = 0;
-    float last_best = -1;
-    for (long kth = 0; kth < K; kth ++) {
+    float last_best = -1.;
+    for (long kth = 0; kth < K; kth++) {
       long besti = 0;
       float best = 1e20;
-      for (long j = tid; j < n; j += block_size) {
+      for (long j = tid; j < m; j += block_size) {
         long index2 = j * 3;
 
         float x_diff = samples[index2] - x1;
@@ -59,7 +58,10 @@ __global__ void furthest_point_sampling_kernel(int n, int m, int K,
         float d = x_diff * x_diff + y_diff * y_diff + z_diff * z_diff;
 
         if (d <= last_best){
-          if (j <= last_besti){
+          if (d < last_best){
+            continue;
+          }
+          else if (j <= last_besti){
             continue;
           }
         }
@@ -85,8 +87,8 @@ __global__ void furthest_point_sampling_kernel(int n, int m, int K,
       max_parallel(4, 2)
       max_parallel(2, 1)
 
-      last_besti = dists_i[0];
       last_best = dists[0];
+      last_besti = dists_i[0];
       if (tid == 0) nearest_index[i * K + kth] = last_besti;
     }
   }
