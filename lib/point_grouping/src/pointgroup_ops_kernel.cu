8f9ab7bed267ceed5aa2b909df582d36ad006a8f
072jiajia <072jiajia@gmail.com>
2021-08-07 20:37:06 +0800

    clean unused

--- lib/point_grouping/src/pointgroup_ops_kernel.cu
+++
@@ -1,6 +0,0 @@
-#include <ATen/ATen.h>
-#include "datatype.h"
-#include "voxelize/voxelize.cu"
-
-template void voxelize_fp_cuda<float>(float *feats, float *output_feats, Int *rules, Int N, Int C);
-template void voxelize_bp_cuda<float>(float *d_output_feats, float *d_feats, Int *rules, Int N, Int C);
